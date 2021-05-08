adb4a86c0d4451faa19b8316c28538bda464008c
Jason Lam <nyjasonlam@gmail.com>
2021-05-08 14:39:29 -0400

    Added Employee class and Manager sub class

--- Assets/utils/employee_classes.js
+++ Assets/utils/employee_classes.js
@@ -5,7 +5,40 @@ class Employee {
     this.name = name;
     this.id = id;
     this.email = email;
+    this.role = 'Employee';
+  }
 
-    
+  getName() {
+    return this.name;
+  }
+
+  getId() {
+    return this.id;
+  }
+  
+  getEmail() {
+    return this.email;
+  }
+
+  getRole() {
+    return this.role
   }
 }
+
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+    this.role = 'Manager';
+  }
+
+  getOfficeNumer() {
+    return this.officeNumber;
+  }
+}
+
+const jason = new Employee('jason', 2, 'nyjasonlam@gmail.com')
+const ben = new Manager('ben', 3, 'ben@bennyandeggs.com', 5)
+
+console.log(jason.getRole())
+console.log(ben.getRole())

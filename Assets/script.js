c79905a64520a550aa459876faed5e23b673a143
Jason Lam <nyjasonlam@gmail.com>
2021-05-08 16:24:38 -0400

    Added logic to push array objects into employeeObjectsArray

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,23 @@
 const inquirer = require("inquirer");
-const { Manager, Engineer, Intern } = require('./utils/employee_classes');
+const { Manager, Engineer, Intern } = require("./utils/employee_classes");
+
+const employeeObjectsArray = [];
 
 const startPrompts = () => {
   inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "name? ",
-    },
-  ])
-  .then((answers) => {
-    console.log(answers);
-  });
-}
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "name? ",
+      },
+    ])
+    .then(({ name }) => {
+      employeeObjectsArray.push(new Engineer(name));
+      console.log(employeeObjectsArray);
+    });
+};
 
-const testEmployee = new Manager('jason', 1, 'nyjasonlam@gmail.com', 1);
+const testEmployee = new Manager("jason", 1, "nyjasonlam@gmail.com", 1);
 
-console.log(testEmployee.getRole())
+startPrompts();

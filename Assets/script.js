e546033f10ef928ba8fd30d03c4b4649ef48f214
Jason Lam <nyjasonlam@gmail.com>
2021-05-09 16:15:50 -0400

    Added logic to repeat menu items until user chooses to end the program

--- Assets/script.js
+++ Assets/script.js
@@ -10,7 +10,7 @@ const {
   optionsList,
 } = require("./utils/inquirer_prompts");
 
-const userInput = new Map().set('employees', []);
+const userInput = new Map().set("employees", []);
 
 const startPrompts = () => {
   let addMoreEmployees = true;
@@ -18,40 +18,50 @@ const startPrompts = () => {
 
   inquirer.prompt(starterQuestion).then(({ teamName }) => {
     userInput.set("teamName", teamName);
-    inquirer.prompt(optionsList).then(({ option }) => {
-      switch (option) {
-        case "Add Manager":
-          return inquirer
-            .prompt(managerQuestions)
-            .then(({ name, id, email, officeNumber }) => {
-              const employeeObject = new Manager(name, id, email, officeNumber)
-              setEmployeeObjectToUserInput(employeeObject)
-            });
-        case "Add Engineer":
-          return inquirer
-            .prompt(engineerQuestions)
-            .then(({ name, id, email, githubName }) => {
-              const employeeObject = new Engineer(name, id, email, githubName)
-              setEmployeeObjectToUserInput(employeeObject)
-            });
-        case "Add Intern":
-          return inquirer
-            .prompt(internQuestions)
-            .then(({ name, id, email, school }) => {
-              const employeeObject = new Intern(name, id, email, school)
-              setEmployeeObjectToUserInput(employeeObject)
-            });
-        case "Finish and Generate HTML":
-          addMoreEmployees = false;
-          userInput.get('employees')
-      }
-    });
+    askEmployeeQuestions();
+  });
+};
+
+const askEmployeeQuestions = () => {
+  inquirer.prompt(optionsList).then(({ option }) => {
+    switch (option) {
+      case "Add Manager":
+        inquirer
+          .prompt(managerQuestions)
+          .then(({ name, id, email, officeNumber }) => {
+            const employeeObject = new Manager(name, id, email, officeNumber);
+            setEmployeeObjectToUserInput(employeeObject);
+            askEmployeeQuestions();
+          });
+        break;
+      case "Add Engineer":
+        inquirer
+          .prompt(engineerQuestions)
+          .then(({ name, id, email, githubName }) => {
+            const employeeObject = new Engineer(name, id, email, githubName);
+            setEmployeeObjectToUserInput(employeeObject);
+            askEmployeeQuestions();
+          });
+        break;
+      case "Add Intern":
+        inquirer.prompt(internQuestions).then(({ name, id, email, school }) => {
+          const employeeObject = new Intern(name, id, email, school);
+          setEmployeeObjectToUserInput(employeeObject);
+          askEmployeeQuestions();
+        });
+        break;
+      case "Finish and Generate HTML":
+        // fs.writeFile('test.html', userInput.get("employees"), "utf8", (err) => {
+        //   err ? console.log(err) : console.log("Successfully written");
+        // });
+        // TODO: Create HTML renderer
+        break;
+    }
   });
 };
 
 const setEmployeeObjectToUserInput = (employeeObject) => {
-  userInput.set('employees', userInput.get('employees').push(employeeObject))
-}
+  userInput.set("employees", [...userInput.get("employees"), employeeObject]);
+};
 
 startPrompts();
-
